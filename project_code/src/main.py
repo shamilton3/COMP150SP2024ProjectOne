afb52fb95958311be6fd41b6b9f25851cf3019be
shamilton3 <156938523+shamilton3@users.noreply.github.com>
2024-03-24 23:46:37 -0500

    Updated the location of the game

--- project_code/src/main.py
+++ project_code/src/main.py
@@ -6,22 +6,36 @@ import random
 
 
 class Location:
+    pass 
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.visited = False
 
-    def __init__(self, parser, number_of_events: int = 1):
-        self.parser = parser
-        self.events = [Event(self.parser) for _ in range(number_of_events)]
+    def __str__(self):
+        return f"{self.name}: {self.description}"
+
+    def visit(self):
+        self.visited = True
+        print(f"You have visited {self.name}.")
+
+    def leave(self):
+        self.visited = False
+        print(f"You leave {self.name}.")
 
 
-    import json
+class WildWestLocation(Location):
+    def __init__(self, name, description, inhabitants=[]):
+        super().__init__(name, description)
+        self.inhabitants = inhabitants
 
-    def create_custom_event_from_static_text_file(self, file_path):
-        # load json file from path
-        with open(file_path, "r") as file:
-            data = json.load(file)
-
-        return Event(self.parser, data)
-
-
+    def describe_inhabitants(self):
+        if self.inhabitants:
+            print(f"The {self.name} is populated by:")
+            for inhabitant in self.inhabitants:
+                print(f"- {inhabitant}")
+        else:
+            print(f"The {self.name} is deserted.")
 
 
 
